7a16d4ecca2c9b17aacd282f2b239963cb651211
BoFo <brunofazio466@gmail.com>
2024-02-01 18:03:06 -0300

    primer commit por ahora el juego solo te deja ingresar los nombre de los jugadores y repartit los mazos y muestra el mazo por consola

---
+++ truco.py
@@ -0,0 +1,70 @@
+import random
+from random import shuffle
+
+class Card:
+    suits = ["Espada", "Basto", "Oro", "Copa"]
+    values = ["anchoEspada", "anchoBasto", "7", "6", "5", "4", "3", "2", "1","anchoOro", "anchoCopa","12","11","10"]
+    
+    def __init__(self, v, s):
+        self.value = v
+        self.suit = s
+
+    def __str__(self):
+        return f"{self.value} de {self.suit}"
+    
+
+
+
+class Deck:
+    def __init__(self):
+        self.cards = [Card(suit, value) for suit in Card.suits for value in Card.values]
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+    
+    def deal(self):
+        return self.cards.pop()
+
+
+
+class Player:
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.mazoCartas = []
+        self.puntos = 0
+        self.victorias = 0
+
+    def agarrarCarta(self, deck):
+        self.mazoCartas.append(deck.deal())
+
+    def play_card(self):
+        return self.mazoCartas.pop
+    
+    def mostrarMazo(self):
+        for card in self.mazoCartas:
+            print(card)
+
+
+class Truco:
+    def __init__(self):
+        nombre1 = input("nombre de jugador 1")
+        nombre2 = input("nombre de jugador 2")
+        self.deck = Deck()
+        self.P1 = Player(nombre1)
+        self.P2 = Player(nombre2)
+
+    def rondaDePrueba(self):
+        for _ in range(3):
+            self.P1.agarrarCarta(self.deck)
+            self.P2.agarrarCarta(self.deck)
+            
+        print(f"Mazo de cartas de {self.P1.nombre}:")
+        self.P1.mostrarMazo()
+
+        print(f"Mazo de cartas de {self.P2.nombre}:")
+        self.P2.mostrarMazo()
+    
+
+if __name__ == "__main__":
+    juego = Truco()
+    juego.rondaDePrueba()
